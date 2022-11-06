01518b79c63528d255e6435346156b9d7b39c1e6
udany <long.eagle.goodies@gmail.com>
2022-11-06 17:20:04 -0300

    Adding a logged in state to the header

--- src/client/layouts/DefaultLayout.vue
+++ src/client/layouts/DefaultLayout.vue
@@ -1,13 +1,17 @@
 <template>
 	<div class="layout-default">
-			<div class="text-center my-3">
-				<router-link to="/">
-					<MainLogo white style="width: 50px" />
-				</router-link>
-			</div>
+		<header :class="{ logged: session.user }">
+			<UserAvatar class="avatar" :user="session.user" interactive />
+
+			<router-link to="/">
+				<MainLogo class="logo" white style="width: 50px" />
+			</router-link>
+		</header>
+
 		<slot></slot>
+
+		<ModalContainer :service="modalService" />
 	</div>
-	<ModalContainer :service="modalService" />
 </template>
 
 <script>
@@ -15,13 +19,17 @@
 	import ModalContainer from 'udany-toolbox/vue/ui/Modal/ModalContainer.vue';
 	import MainContent from '../components/layout/MainContent.vue';
 	import MainLogo from '../components/MainLogo.vue';
+	import { sessionService } from '../services/session';
+	import UserAvatar from '../components/UserAvatar.vue';
 
 	export default {
 		name: 'DefaultLayout',
 		data: () => ({
-			modalService
+			modalService,
+			session: sessionService.session
 		}),
 		components: {
+			UserAvatar,
 			MainLogo,
 			MainContent,
 			ModalContainer
@@ -30,6 +38,49 @@
 </script>
 
 <style lang="scss" scoped>
+	header {
+		--bezier: cubic-bezier(.63,-0.05,.44,1.41);
+		max-width: 768px;
+		text-align: left;
+		margin: var(--spacer-3) auto;
+		position: relative;
+
+		height: 50px;
+
+		&.logged {
+			.logo {
+				left: 0;
+				transform: translateX(0);
+			}
+			.avatar {
+				left: 100%;
+				transform: translateX(-100%);
+
+				opacity: 1;
+				pointer-events: all;
+			}
+		}
+	}
+
+	.logo {
+		position: absolute;
+		left: 50%;
+
+		transition: left .6s var(--bezier), transform .6s;
+		transform: translateX(-50%);
+	}
+
+	.avatar {
+		position: absolute;
+		left: 50%;
+
+		opacity: 0;
+		pointer-events: none;
+
+		transition: left .6s var(--bezier), transform .6s, opacity .6s;
+		transform: translateX(-50%);
+	}
+
 	.layout-default {
 	}
 </style>
