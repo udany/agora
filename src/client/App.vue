71defabf1ca62955ad67a975a77a74e586cc6ac7
udany <long.eagle.goodies@gmail.com>
2022-11-06 23:29:25 -0300

    Route transitions

--- src/client/App.vue
+++ src/client/App.vue
@@ -1,6 +1,10 @@
 <template>
 	<component class="base-layout" :is="layout">
-		<router-view />
+		<router-view v-slot="{ Component }">
+			<transition name="route">
+				<component :is="Component" />
+			</transition>
+		</router-view>
 	</component>
 </template>
 
@@ -26,5 +30,25 @@
 	.base-layout {
 		-webkit-font-smoothing: antialiased;
 		-moz-osx-font-smoothing: grayscale;
+	}
+
+	.route-enter-active,
+	.route-leave-active {
+		transition: opacity .2s ease, transform .4s ease;
+	}
+
+	.route-leave-active {
+		position: absolute;
+	}
+
+	.route-leave-to {
+	}
+	.route-enter-from {
+		transform: translateY(-20px);
+	}
+
+	.route-enter-from,
+	.route-leave-to {
+		opacity: 0;
 	}
 </style>
