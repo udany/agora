c9a41d9a7323e2d15ec68596abf2ee79284f16e5
udany <long.eagle.goodies@gmail.com>
2022-11-08 13:58:00 -0300

    Route leave indicator

--- src/client/components/layout/MainContent.vue
+++ src/client/components/layout/MainContent.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="outer">
-		<div class="main-content">
+		<div class="main-content" :class="{ leaving }">
 			<div class="inner">
 				<slot></slot>
 			</div>
@@ -9,9 +9,20 @@
 </template>
 
 <script lang="ts">
+	import { onBeforeRouteLeave } from 'vue-router';
+	import { ref } from 'vue';
+
 	export default {
 		name: 'MainContent',
 		setup() {
+			let leaving = ref(false);
+			onBeforeRouteLeave(() => {
+				leaving.value = true;
+			});
+
+			return {
+				leaving
+			}
 		}
 	}
 </script>
@@ -20,10 +31,19 @@
 	.outer {
 		width: 100%;
 	}
+
 	.main-content {
 		max-width: 768px;
 		background: var(--neutral-dark);
 		margin: var(--spacer-3) auto;
+
+
+		transition: opacity .3s, filter .3s;
+		&.leaving {
+			opacity: .5;
+			filter: blur(2px) grayscale(1);
+			pointer-events: none;
+		}
 	}
 
 	.inner {
