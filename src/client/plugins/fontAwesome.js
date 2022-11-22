6146555c20e39a2c1f2b3e02fa94a2667652797d
udany <long.eagle.goodies@gmail.com>
2022-11-22 20:59:47 -0300

    Making sure font awesome css gets loaded synchronously

--- src/client/plugins/fontAwesome.js
+++ src/client/plugins/fontAwesome.js
@@ -1,4 +1,5 @@
-import '@fortawesome/fontawesome-svg-core'
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
 import '@fortawesome/vue-fontawesome';
 import fontAwesomePlugin from 'udany-toolbox/vue/ui/FontAwesome/plugin.ts';
 
@@ -25,6 +26,7 @@ import {
 } from '@fortawesome/free-solid-svg-icons'
 
 export default function (app) {
+	config.autoAddCss = false;
 	fontAwesomePlugin(app, [
 		faTimes,
 		faQuestion,
